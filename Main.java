d6aeabb32f3ea21572815cd7782b357ed9ac5093
Shourov Foisal <shourovfoisal@gmail.com>
2023-05-09 17:55:41 +0600

    Implemented class, interface and lambda expression based Threads.

--- Main.java
+++ Main.java
@@ -1,5 +1,47 @@
+
+class MyThread1 extends Thread {
+    @Override
+    public void run() {
+        for(int i=0; i<2500; i++) {
+            System.out.println("Thread 1 is running.");
+        }
+    }
+}
+
+class MyThread2 extends Thread {
+    @Override
+    public void run() {
+        for (int i=0; i<2500; i++) {
+            System.out.println("Thread 2 is running.");
+        }
+    }
+}
+
+class MyThread3 implements Runnable {
+    @Override
+    public void run() {
+        for(int i=0; i<2500; i++) {
+            System.out.println("Thread 3 is running.");
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        MyThread1 t1 = new MyThread1();
+        MyThread2 t2 = new MyThread2();
+        MyThread3 t3 = new MyThread3();
+
+        Runnable t4 = () -> {
+            for(int i=0; i<2500; i++) {
+                System.out.println("Thread 4 is running.");
+            }
+        };
+
+
+        t1.start();
+        t2.start();
+        t3.run();
     }
 }
---
+++ out/production/multithreading-java/README.md
@@ -0,0 +1,4 @@
+# Learning Multithreading
+
+__Tutorial:__ _Creating a Thread by Extending Thread class_\
+[https://www.youtube.com/watch?v=b_h4-_j6JmY&ab_channel=CodeWithHarry](https://www.youtube.com/watch?v=b_h4-_j6JmY&ab_channel=CodeWithHarry)
